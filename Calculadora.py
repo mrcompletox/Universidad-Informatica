bbfb415ff8959cf7d8ed1856df43d20a548d315c
Martin Donoso <martindonoso1987@gmail.com>
2026-06-03 21:35:44 -0400

    Calculadora muy sencilla

---
+++ Calculadora.py
@@ -0,0 +1,15 @@
+# leer dos numeros
+num1 = float(input("N°1:"))
+num2 = float(input("N°2:"))
+
+#operaciones
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+#resultado
+print("Suma:",suma)
+print("Resta:",resta)
+print("Multiplicación:",multiplicacion)
+print("División:",division)
